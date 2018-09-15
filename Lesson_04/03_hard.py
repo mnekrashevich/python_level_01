49965be133eb612de2557b5e702dcd6811a5be63
mnekrashevich <mnekrashevich@gmail.com>
2018-09-15 19:33:46 +0300

    решение задачи уровня hard четвертого урока

--- Lesson_04/03_hard.py
+++ Lesson_04/03_hard.py
@@ -31,7 +31,7 @@ def check_account(person):
 
 
 def withdraw_money(person, money):
-    if person['money'] - money == 0:
+    if person['money'] - money >= 0:
         person['money'] -= money
         return 'Вы сняли {} рублей.'.format(money)
     else:
@@ -39,32 +39,48 @@ def withdraw_money(person, money):
 
 
 def process_user_choice(choice, person):
-    if choice == '1':
+    if choice == 1:
         print(check_account(person))
-    elif choice == '2':
-        count = float(input('Сумма к снятию:'))
-        print(withdraw_money(person, count))
+    elif choice == 2:
+        try:
+            count = float(input('Сумма к снятию:'))
+            if round(count,2) != count:
+                raise ValueError
+            print(withdraw_money(person, count))
+        except ValueError:
+            print('Сумма введена неверно. Введите число с точностью до двух знаков')
+
+    else:
+        print('Неизвестная опция')
 
 
 def start():
-    card_number, pin_code = input('Введите номер карты и пин код через пробел:').split()
+    try:
+        card_number, pin_code = input('Введите номер карты и пин код через пробел:').split()
 
-    card_number = int(card_number)
-    pin_code = int(pin_code)
-    person = get_person_by_card(card_number)
-    if person and is_pin_valid(person, pin_code):
-        while True:
-            choice = int(input('Выберите пункт:\n'
-                               '1. Проверить баланс\n'
-                               '2. Снять деньги\n'
-                               '3. Выход\n'
-                               '---------------------\n'
-                               'Ваш выбор:'))
-            if choice == 3:
-                break
-            process_user_choice(choice, person)
+        card_number = int(card_number)
+        pin_code = int(pin_code)
+    except ValueError:
+        print('Данные введены неверно:\n'
+              'Допустимо использовать только две группы цифр разделенных пробелом')
     else:
-        print('Номер карты или пин код введены не верно!')
-
+        person = get_person_by_card(card_number)
+        if person and is_pin_valid(person, pin_code):
+            while True:
+                try:
+                    choice = int(input('Выберите пункт:\n'
+                                       '1. Проверить баланс\n'
+                                       '2. Снять деньги\n'
+                                       '3. Выход\n'
+                                       '---------------------\n'
+                                       'Ваш выбор:'))
+                except ValueError:
+                    print('При выборе варианта допустимы только цифры')
+                    choice = -1
+                if choice == 3:
+                    break
+                process_user_choice(choice, person)
+        else:
+            print('Номер карты или пин код введены не верно!')
 
 start()
